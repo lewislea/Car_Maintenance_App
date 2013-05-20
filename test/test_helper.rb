58a25d8abb8bef6f4168faaf4dedd4dc44a80c7f
lewislea <wlewislea@gmail.com>
2013-05-20 15:51:00 -0500

    add test database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,10 @@
 require 'test/unit'
 require_relative '../bootstrap_ar'
 
+connect_to 'test'
+
+ENV['FP_ENV'] = 'test'
+
 module DatabaseCleaner
   def before_setup
     super
-    Project.destroy_all
-  end
-end
