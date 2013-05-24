742bc810d75038bc3eaf40b873eae07fbf5df66d
lewislea <wlewislea@gmail.com>
2013-05-24 14:47:12 -0500

    add repair, list, remove vehicle and repair working.

--- car.rb
+++ car.rb
@@ -1,23 +1,21 @@
  #!/usr/bin/env ruby
 
 require './bootstrap_ar'
-# require_relative 'views/command_list'
 
 database = ENV['CAR_ENV'] || 'development'
+
 connect_to database
-# welcome = Launch.commands_list
+
 welcome = "\nWelcome to the Car Maintenance App!\n
   What would you like to do?\n
-  - to add a vehicle to the database, type 'add-vehicle'
-  - to add a repair, type 'add-repair'
-  - to view a list of vehicles, type 'list-vehicles'\n"
+  - to add a vehicle to the database, type 'add-v'
+  - to add a repair, type 'add-r'
+  - to view a list of vehicles, type 'list'\n"
 puts welcome
 
-# ARGV = [add, foo]
-# command = ARGV[0]
 command = gets.chomp
 
-if command == "add-vehicle"
+if command == "add-v"
   puts "What is the make of the vehicle?"
   make = gets.chomp
   puts "What is the model of the vehicle"
@@ -34,20 +32,21 @@ if command == "add-vehicle"
   else
     puts "Failure: #{vehicle.errors.full_messages.join(", ")}"
   end
-elsif command == "add-repair"
+elsif command == "add-r"
   puts "What type of repair would you like to log? (We'll assign it to a vehicle at the end."
   repair_type = gets.chomp
   puts "How much did the repair cost?"
   repair_cost = gets.chomp
   puts "Which mechanic did the repair?"
   mechanic = gets.chomp
-  puts "What was the date of the repair?"
+  puts "What was the date of the repair? YYYY/MM/DD"
   date_of_repair = gets.chomp
   puts "What mileage was on the vehicle at the time of the repair?"
   mileage_at_time = gets.chomp
   puts "Please type any other notes that you would like to document the repair."
   repair_notes = gets.chomp
   puts "Which vehicle would you like to add a repair to?"
+  #controller for all the listing stuff?
   all_vehicles = Vehicle.all
   all_vehicles.each_with_index do |vehicle, i|
     puts "#{i+1}. #{vehicle.year} #{vehicle.make} #{vehicle.model} vehicle ID: #{vehicle.id}"
@@ -55,26 +54,47 @@ elsif command == "add-repair"
   puts "type the ID of the vehicle that the repair is to be added to"
   vehicle_id = gets.chomp
   # vehicle = Vehicle.find(vehicle_id)
-
-  new_repair = Repair.new(repair_type: repair_type, repair_cost: repair_cost, mechanic: mechanic,
-    date_of_repair: date_of_repair, mileage_at_time_of_repair: mileage_at_time, repair_notes: repair_notes,
+  new_repair = Repair.new(type: repair_type, cost: repair_cost, mechanic: mechanic,
+    date: date_of_repair, mileage: mileage_at_time, notes: repair_notes,
     vehicle_id: vehicle_id)
   if new_repair.save
     puts "repair added!"
   else
-    puts "Failure: #{repair.errors.full_messages.join(", ")}"
+    puts "We were not able to add your repair. Please start the program again."
   end
-else command == "list-vehicles"
-  puts "\nTo view repairs for a vehicle, type __\n To remove a vehicle from the list, type
-  ____.\nTo add a vehicle, type ____\n"
-  # all_vehicles = Vehicle.all
-  # vehicle.each_with_index do |vehicle, i|
-  #   puts "#{i+1}. #{vehicle.make} vehicle ID: #{vehicle.id}"
+else command == "list"
+  all_vehicles = Vehicle.all
+  all_vehicles.each_with_index do |vehicle, i|
+    puts "#{i+1}. #{vehicle.year} #{vehicle.make} #{vehicle.model} vehicle ID: #{vehicle.id}"
+    end
+  puts "\n- to view repairs for a vehicle, type view-r\n- to remove a vehicle from the list, type remove-vehicle\n- to add a vehicle, type ____\n"
+  new_command = gets.chomp
+    if new_command == "add-vehicle"
+      #somehow get back to add-vehicle command - controller?
+    elsif new_command == "view-r"
+      all_vehicles.each_with_index do |vehicle|
+      puts "vehicle ID: #{vehicle.id} #{vehicle.year} #{vehicle.make} #{vehicle.model} "
+      end
+      puts "type the ID number of vehicle to view its repairs"
+      vehicle_id = gets.chomp
+      matching_repairs = Repair.where(vehicle_id: vehicle_id).all
+      matching_repairs.each do |repair,|
+        puts " date: #{repair.date_of_repair} repair: #{repair.repair_type} cost: #{repair.repair_cost}"
+      end
+    else new_command == "remove-vehicle"
+      all_vehicles = Vehicle.all
+      all_vehicles.each_with_index do |vehicle|
+      puts "vehicle ID: #{vehicle.id} #{vehicle.year} #{vehicle.make} #{vehicle.model} "
+      end
+      puts "Please choose the vehicle you would like to remove and type the ID below\n
+      NOTE: all repairs associated with this vehicle will also be removed."
+      vehicle_id = gets.chomp
+      # matching_repair = Repair.where(vehicle_id: vehicle_id)
+      if Vehicle.delete(vehicle_id)
+        Repair.where(:vehicle_id => vehicle_id).destroy_all
+        puts "vehicle removed!"
+      else
+        puts "vehicle was not able to be removed"
+      end
+    end
 end
-# elsif command == "remove"
-#   project_name = ARGV[1]
-#   matching_projects = Project.where(name: project_name).all
-#   matching_projects.each do |project|
-#     project.destroy
-  # end
-# end
