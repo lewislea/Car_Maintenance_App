9e6f7fd4f2eeb4bd0cbdf38ee6388f5c5514fb20
lewislea <wlewislea@gmail.com>
2013-05-27 23:43:03 -0500

    slightly updated tests

--- test/test_adding_repair.rb
+++ test/test_adding_repair.rb
@@ -5,7 +5,8 @@ class TestAddingRepair < MiniTest::Unit::TestCase
 
 def test_saves_to_database
     assert_equal 0, Repair.count
-    vehicle = Repair.new(type: "brakes", cost: 40, mechanic: "gary")
+    vehicle = Repair.new(repair_type: "brakes", cost: 40, mechanic: "gary",
+    vehicle_id: 2, mileage: 2, date: 2012-3-4)
     vehicle.save!
     assert_equal 1, Repair.count
 end
--- test/test_adding_vehicle.rb
+++ test/test_adding_vehicle.rb
@@ -5,38 +5,21 @@ class TestAddingVehicle < MiniTest::Unit::TestCase
 
   def test_saves_to_database
     assert_equal 0, Vehicle.count
-    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986)
+    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986, mileage: 20)
     vehicle.save!
     assert_equal 1, Vehicle.count
   end
 
   def test_add_new_vehicle_make
-    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986)
+    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986, mileage: 20)
     vehicle.save!
     assert_equal("Honda", vehicle.make)
   end
 
   def test_add_new_vehicle_model
-    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986)
+    vehicle = Vehicle.new(make: "Honda", model: "Civic", year: 1986, mileage: 20)
     vehicle.save!
     assert_equal("Civic", vehicle.model)
   end
 
-  # def test_takes_arguments_and_uses_them
-  #   `ruby futureperfect add foo`
-  #   assert_equal 'foo', Project.last.name
-  # end
-
-  # def test_duplicate_names_are_ignored
-  #   Project.create( name: 'foo' )
-  #   original_project_count = Project.count
-  #   `ruby futureperfect add foo`
-  #   assert_equal original_project_count, Project.count
-  # end
-
-  # def test_duplicate_names_outputs_error_message
-  #   Project.create( name: 'foo' )
-  #   results = `ruby futureperfect add foo`
-  #   assert results.include?('Name must be unique'), "Actually was '#{results}'"
-  # end
 end
